662e0949fc95c77902127291118189990fb1d10c
ynvYauneEnovore <yovanuxf@gmail.com>
2023-05-09 00:18:40 -0400

    update-09-05-2023-00-18

--- resources/views/login/main.blade.php
+++ resources/views/login/main.blade.php
@@ -1,7 +1,7 @@
 @extends('../layout/' . $layout)
 
 @section('head')
-    <title>Login - Rubick - Tailwind HTML Admin Template</title>
+    <title>Acceso</title>
 @endsection
 
 @section('content')
@@ -10,44 +10,43 @@
             <!-- BEGIN: Login Info -->
             <div class="hidden xl:flex flex-col min-h-screen">
                 <a href="" class="-intro-x flex items-center pt-5">
-                    <img alt="Midone - HTML Admin Template" class="w-6" src="{{ asset('dist/images/logo.svg') }}">
+                    <img alt="mancuernas-en-x" class="w-10" src="{{ asset('img/logo-1.png') }}">
                     <span class="text-white text-lg ml-3">
-                        Rubick
+                        aGYMsa
                     </span>
                 </a>
                 <div class="my-auto">
-                    <img alt="Midone - HTML Admin Template" class="-intro-x w-1/2 -mt-16" src="{{ asset('dist/images/illustration.svg') }}">
-                    <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">A few more clicks to <br> sign in to your account.</div>
-                    <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Manage all your e-commerce accounts in one place</div>
+                    <img alt="logo_tiburon_furioso" class="-intro-x w-1/2 -mt-20" src="{{ asset('img/logo.png') }}">
+                    <div class="-intro-x text-white font-medium text-4xl leading-tight mt-10">Únete a nosotros para un <br> estilo de vida más saludable.</div>
+                    <div class="-intro-x mt-5 text-lg text-white text-opacity-70 dark:text-slate-400">Agymsa &copy; 2023</div>
                 </div>
             </div>
             <!-- END: Login Info -->
             <!-- BEGIN: Login Form -->
             <div class="h-screen xl:h-auto flex py-5 xl:py-0 my-10 xl:my-0">
                 <div class="my-auto mx-auto xl:ml-20 bg-white dark:bg-darkmode-600 xl:bg-transparent px-5 sm:px-8 py-8 xl:p-0 rounded-md shadow-md xl:shadow-none w-full sm:w-3/4 lg:w-2/4 xl:w-auto">
-                    <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">Sign In</h2>
+                    <h2 class="intro-x font-bold text-2xl xl:text-3xl text-center xl:text-left">Inicio de Sesión</h2>
                     <div class="intro-x mt-2 text-slate-400 xl:hidden text-center">A few more clicks to sign in to your account. Manage all your e-commerce accounts in one place</div>
                     <div class="intro-x mt-8">
                         <form id="login-form">
-                            <input id="email" type="text" class="intro-x login__input form-control py-3 px-4 block" placeholder="Email" value="midone@left4code.com">
+                            <input id="email" type="text" class="intro-x login__input form-control py-3 px-4 block" placeholder="Nombre de Usuario" value="">
                             <div id="error-email" class="login__input-error text-danger mt-2"></div>
-                            <input id="password" type="password" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="Password" value="password">
+                            <input id="password" type="password" class="intro-x login__input form-control py-3 px-4 block mt-4" placeholder="contraseña" value="">
                             <div id="error-password" class="login__input-error text-danger mt-2"></div>
                         </form>
                     </div>
                     <div class="intro-x flex text-slate-600 dark:text-slate-500 text-xs sm:text-sm mt-4">
                         <div class="flex items-center mr-auto">
                             <input id="remember-me" type="checkbox" class="form-check-input border mr-2">
-                            <label class="cursor-pointer select-none" for="remember-me">Remember me</label>
+                            <label class="cursor-pointer select-none" for="remember-me">Recuérdame</label>
                         </div>
-                        <a href="">Forgot Password?</a>
+                        <a href="">¿Olvidaste tu contraseña?</a>
                     </div>
                     <div class="intro-x mt-5 xl:mt-8 text-center xl:text-left">
-                        <button id="btn-login" class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Login</button>
-                        <button class="btn btn-outline-secondary py-3 px-4 w-full xl:w-32 mt-3 xl:mt-0 align-top">Register</button>
+                        <button id="btn-login" class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Acceder</button>
                     </div>
                     <div class="intro-x mt-10 xl:mt-24 text-slate-600 dark:text-slate-500 text-center xl:text-left">
-                        By signin up, you agree to our <a class="text-primary dark:text-slate-200" href="">Terms and Conditions</a> & <a class="text-primary dark:text-slate-200" href="">Privacy Policy</a>
+                        Al acceder, estás de acuerdo con nuestros <a class="text-primary dark:text-slate-200" href="">Términos y Condiciones.</a> & <a class="text-primary dark:text-slate-200" href="">Privacy Policy</a>
                     </div>
                 </div>
             </div>
